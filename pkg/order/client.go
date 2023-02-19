6a36d67cce48347eea151c6a075de59f80bb6316
dedi hartono <dedi.hartono@dagangan.com>
2023-02-19 18:20:40 +0700

    some changes

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -2,10 +2,12 @@ package order
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/dedihartono801/api-gateway/pkg/config"
 	pb "github.com/dedihartono801/protobuf/order/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type ServiceClient struct {
@@ -13,8 +15,23 @@ type ServiceClient struct {
 }
 
 func InitServiceClient(c *config.Config) pb.OrderServiceClient {
-	// using WithInsecure() because no SSL running
-	cc, err := grpc.Dial(c.OrderSvcUrl, grpc.WithInsecure())
+	opts := []grpc.DialOption{}
+	tls := true
+
+	if tls {
+		certFile := "../ssl/order-svc/ca.crt"
+
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificates: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	// using WithSecure() because no SSL running
+	cc, err := grpc.Dial(c.OrderSvcUrl, opts...)
 
 	if err != nil {
 		fmt.Println("Could not connect:", err)
